42b2d0729c6f70e9bf7181cf6665d665064a20e6
heliagandhi <38745908+heliagandhi@users.noreply.github.com>
2021-05-11 20:45:25 +0700

    Swagger API Operations Ordering

--- src/main/java/com/Teknisi/config/TeknisiConfig.java
+++ src/main/java/com/Teknisi/config/TeknisiConfig.java
@@ -9,6 +9,9 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -25,6 +28,14 @@ public class TeknisiConfig {
 				.paths(PathSelectors.regex("/.*"))
 				.build()
 				;
+	}
+	
+	@Bean
+	public UiConfiguration uiConfig() {
+	    return UiConfigurationBuilder
+	            .builder()
+	            .operationsSorter(OperationsSorter.METHOD)
+	            .build();
 	}
 
 	private ApiInfo apiEndInfo() {
