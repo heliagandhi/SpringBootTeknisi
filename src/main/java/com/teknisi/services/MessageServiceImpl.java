fadfce1ddfe54764bc0a79c24f3489d2c9dc83a8
heliagandhi <38745908+heliagandhi@users.noreply.github.com>
2021-06-07 13:38:12 +0700

    annotation value

--- src/main/java/com/teknisi/services/MessageServiceImpl.java
+++ src/main/java/com/teknisi/services/MessageServiceImpl.java
@@ -17,29 +17,10 @@ import com.teknisi.model.AppUser;
 public class MessageServiceImpl implements MessageService{
 	@Autowired private JavaMailSender javaMailSender;
 	
-	@Value("${template.mail.subject: Testing from SpringBoot Teknisi}")
+	@Value("${template.mail.subject}")
 	private String subject;
 	
-	@Value("${template.mail.message: <table width=\"100%\">\r\n"
-			+ "  <tr>\r\n"
-			+ "    <td align=\"center\"><img src=\"https://llndcz.stripocdn.email/content/guids/CABINET_2663efe83689b9bda1312f85374f56d2/images/10381620386430630.png\" width=\"100\">\r\n"
-			+ "    </td>\r\n"
-			+ "  </tr>\r\n"
-			+ "  <tr>\r\n"
-			+ "    <td align=\"center\"><h2 style=\"Margin:0;line-height:43px;mso-line-height-rule:exactly;font-family:Montserrat, sans-serif;font-size:36px;font-style:normal;font-weight:normal;color:#333333\\\">Thank you!! <br /> you\r\n"
-			+ "    have successfully registered</h2>\r\n"
-			+ "    </td>\r\n"
-			+ "  </tr>\r\n"
-			+ "  <tr>\r\n"
-			+ "    <td align=\"center\">\r\n"
-			+ "      <table border=\"0\" width=\"40%\" height=\"100%\" style=\"border-spacing:0px;width:40% !important;display:inline-table\\\">\r\n"
-			+ "        <tr>\r\n"
-			+ "        	<td style=\"padding:1;Margin:0;border-bottom:1px solid #CCCCCC;background:none;height:2px;width:100%;margin:0px\"></td>\r\n"
-			+ "        </tr>\r\n"
-			+ "      </table>\r\n"
-			+ "    </td>\r\n"
-			+ "  </tr>\r\n"
-			+ "</table>}")
+	@Value("${template.mail.message}")
 	private String message;
 
 	@Override
