ad5dbf1e447b06fd0483ab5260b8a737aa36e0d4
heliagandhi <38745908+heliagandhi@users.noreply.github.com>
2021-06-16 12:55:58 +0700

    Thymeleaf

--- src/main/java/com/teknisi/scheduler/Scheduler.java
+++ src/main/java/com/teknisi/scheduler/Scheduler.java
@@ -39,7 +39,7 @@ public class Scheduler {
 	@Autowired AppUserService appUserService;
 	@Autowired FileService fileService;
 	
-//	@Scheduled(cron = "0 0/10 * * * *")
+	@Scheduled(cron = "0 0/10 * * * *")
 	public void sendEmailRequestStatusNew() {
 		List<Request> listRequest = requestService.getAllStatusRequest("NEW");
 		for (Request request : listRequest) {
@@ -60,7 +60,7 @@ public class Scheduler {
 		logger.info("Schedule reminder for request status = NEW has been sent to email => " + dateFormat.format(new Date()));
 	}
 	
-//	@Scheduled(fixedRate = 300000)
+	@Scheduled(fixedRate = 300000)
 	public void sendEmailRequestStatusMailSent() throws ParseException, java.text.ParseException {
 		logger.info("Check all request that has status mail_sent");
 		List<Request> listRequest = requestService.getRequestByBeforeDate("MAIL_SENT");
@@ -80,7 +80,7 @@ public class Scheduler {
 	}
 	
 
-//	@Scheduled(cron = "0 0 12 * * 1-5")
+	@Scheduled(cron = "0 0 12 * * 1-5")
 	public void emailAllPendingStatus() throws IOException, MessagingException {
 		logger.info("Check all request that has status MAIL_SENT, NEW and PROSSESED");
 		fileService.exportToCSV();
@@ -93,7 +93,7 @@ public class Scheduler {
 		logger.info("Schedule information for pending request has been sent to admin email");
 	}
 	
-//	@Scheduled(cron = "0 0 17 * * 1-5")
+	@Scheduled(cron = "0 0 17 * * 1-5")
 	public void emailReportAllFinishedStatus() throws IOException, MessagingException, JRException {
 		logger.info("Check all ticket request that has status Finished");
 		logger.info("Exporting all data to PDF");
@@ -106,8 +106,8 @@ public class Scheduler {
 		logger.info("Schedule report for finished ticket request has been sent to admin email");
 	}
 	
-//	@Scheduled(cron = "0 0 18 * * 5")
-	@Scheduled(cron = "5 * * * * *")
+	@Scheduled(cron = "0 0 18 * * 5")
+//	@Scheduled(cron = "5 * * * * *")
 	public void emailRecapitulationReport2() throws IOException, MessagingException, JRException {
 		logger.info("Check all ticket request for a recapitulation");
 		logger.info("Exporting all data to XLS");
